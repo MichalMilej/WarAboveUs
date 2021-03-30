4ae064f35a5435ca8de0df19e88de9aace8841aa
mfalkiewicz102171 <80722058+mfalkiewicz102171@users.noreply.github.com>
2021-03-30 13:17:41 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+    Controller controller;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("War Above Us");
+        primaryStage.setScene(new Scene(root, 960, 540));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
